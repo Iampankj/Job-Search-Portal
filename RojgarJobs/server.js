099463a7ead7f8d72ac4b1511cf93b100bb80e12
Pankaj Shah <Pankaj123.pks@gmail.com>
2022-11-16 14:47:05 +0100

    setting server.js and mongoDB for database

--- RojgarJobs/server.js
+++ RojgarJobs/server.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express();
 const db = require("./db.js")
+const jobsRoute = require("./routes/jobsRoute")
+app.use('/api/jobs', jobsRoute)
+
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log('Node JS server started'));
